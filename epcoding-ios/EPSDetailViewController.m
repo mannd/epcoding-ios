10420722b339a254760c7b5b418f355fc3619261
David Mann <mannd@epstudiossoftware.com>
2014-02-21 10:33:45 +0100

    added tableview to detail view

--- epcoding-ios/EPSDetailViewController.m
+++ epcoding-ios/EPSDetailViewController.m
@@ -36,7 +36,6 @@
     // Update the user interface for the detail item.
 
     if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
         self.title = _detailItem;
     }
 }
