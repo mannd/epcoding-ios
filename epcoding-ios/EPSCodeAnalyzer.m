c1e5716418bab68bb99e500e154a0644f1ea1aa0
David Mann <mannd@epstudiossoftware.com>
2014-11-13 21:38:43 -0700

    re-edited IV drug infusion code back to original, fixed iso use in analyzer

--- epcoding-ios/EPSCodeAnalyzer.m
+++ epcoding-ios/EPSCodeAnalyzer.m
@@ -75,7 +75,7 @@
     [array addObject:[[EPSCodeError alloc] initWithCodes:[NSMutableArray arrayWithArray:@[@"93654", @"93657", @"93609", @"93613", @"93622"]] withWarningLevel:ERROR withMessage:@"Code(s) cannot be add to VT Ablation."]];
     [array addObject:[[EPSCodeError alloc] initWithCodes:[NSMutableArray arrayWithArray:@[@"93650", @"93609", @"93613"]] withWarningLevel:WARNING withMessage:@"It is not clear if mapping codes can be combined with AV node ablaton."]];
     [array addObject:[[EPSCodeError alloc] initWithCodes:[NSMutableArray arrayWithArray:@[@"93650", @"93600", @"93619", @"93620"]] withWarningLevel:WARNING withMessage:@"It is unclear if AV node ablation can be combined with EP testing codes."]];
-    [array addObject:[[EPSCodeError alloc] initWithCodes:[NSMutableArray arrayWithArray:@[@"93650", @"93653", @"93654", @"93656", @"93623"]] withWarningLevel:WARNING withMessage:@"Induce post IV drug (e.g. isoproterenol) can be added if used prior to ablation.  It can't be added if only used post-ablation to test ablation efficacy."]];
+    [array addObject:[[EPSCodeError alloc] initWithCodes:[NSMutableArray arrayWithArray:@[@"93623", @"93650", @"93653", @"93654", @"93656"]] withWarningLevel:WARNING withMessage:@"Induce post IV drug only OK if used pre-ablation, not just post-ablation."]];
 
     
     return array;
