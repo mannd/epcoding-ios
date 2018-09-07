8156c1b2fbd9b9bff6ff6517a6ba9d8411468774
David Mann <mannd@epstudiossoftware.com>
2018-09-06 18:42:30 -0600

    Get version from bundle

--- epcoding-ios/EPSAbout.m
+++ epcoding-ios/EPSAbout.m
@@ -9,13 +9,10 @@
 #import "EPSAbout.h"
 #import <UIKit/UIKit.h>
 
-// TODO: update version
-#define VERSION @"1.10"
-
 @implementation EPSAbout
 
 + (void)show:(UIViewController *)vc {
-    NSString *details = [[NSString alloc] initWithFormat: @"Copyright \u00a9 2013-2017 EP Studios, Inc.\nAll rights reserved.\nVersion %@" , VERSION];
+    NSString *details = [[NSString alloc] initWithFormat: @"Copyright \u00a9 2013-2017 EP Studios, Inc.\nAll rights reserved.\nVersion %@" , [self getVersion]];
     NSString *title = @"EP Coding";
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:details preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
@@ -23,6 +20,18 @@
     [vc presentViewController:alert animated:YES completion:nil];
 }
 
++ (NSString *)getVersion {
+    NSDictionary *dictionary = [[NSBundle mainBundle] infoDictionary];
+    NSString *version = dictionary[@"CFBundleShortVersionString"];
+#ifdef DEBUG // Get build number, if you want it. Cleaner to leave out of release version.
+    NSString *build = dictionary[@"CFBundleVersion"];
+    // the version+build format is recommended by https://semver.org
+    NSString *versionBuild = [NSString stringWithFormat:@"%@+%@", version, build];
+    return versionBuild;
+#else
+    return version;
+#endif
+}
 
 
 @end
